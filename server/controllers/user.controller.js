a99b0a02bf37488dc219d1c2fdf1d1f431eec3a6
Nitish Nashine <nitishnashine@gmail.com>
2024-12-27 16:29:19 +0530

    add login route and controller with validation and JWT generation

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -16,3 +16,29 @@ export const createUserController = async (req,res) => {
         return res.status(500).json({error:error.message});
     }
 }
+
+export const loginUserController = async (req,res) => {
+    const errors = validationResult(req.body);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    try {
+        const {email,password} = req.body;
+        const user = await userModel.findOne({email}).select("+password");
+        if(!user){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+
+        const isMatch = await user.isValidPassword(password);
+        if(!isMatch){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+
+        const token = await user.generateJWT();
+        return res.status(200).json({user,token,message:"User logged in successfully"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:error.message});
+    }
+}
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -26,6 +26,7 @@ userSchema.statics.hashPassword = async function(password){
 }
 
 userSchema.methods.isValidPassword = async function(password){
+    console.log(password,this.password);
     return await bcrypt.compare(password,this.password);
 }
 
