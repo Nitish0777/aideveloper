a2b57fb309df733abba443bd7b5e18d9ac98358d
Nitish Nashine <nitishnashine@gmail.com>
2024-12-28 11:03:28 +0530

    implement login and registration forms with state management and API integration

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,30 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../config/axios.js";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      axios
+        .post("/users/login", { email, password })
+        .then((res) => {
+          console.log(res.data);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-sm font-medium mb-2" htmlFor="email">
               Email
             </label>
             <input
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               id="email"
+              value={email}
               className="w-full p-3 rounded bg-gray-700 border border-gray-600 focus:outline-none focus:border-blue-500"
               required
             />
           </div>
           <div className="mb-6">
-            <label className="block text-sm font-medium mb-2" htmlFor="password">
+            <label
+              className="block text-sm font-medium mb-2"
+              htmlFor="password"
+            >
               Password
             </label>
             <input
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               id="password"
+              value={password}
               className="w-full p-3 rounded bg-gray-700 border border-gray-600 focus:outline-none focus:border-blue-500"
               required
             />
--- server/app.js
+++ server/app.js
@@ -3,11 +3,18 @@ import morgan from "morgan";
 import connectDB from "./db/db.js";
 import userRoute from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 connectDB();
 
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true,
+  }
+))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
