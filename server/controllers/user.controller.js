a78f4e61b44f9e56eb14b0a6dfc7fc9966633b85
Nitish Nashine <nitishnashine@gmail.com>
2024-12-28 11:11:22 +0530

    remove password from user object before sending response in create and login controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,6 +12,7 @@ export const createUserController = async (req, res) => {
     const { email, password } = req.body;
     const user = await userService.createUser(email, password);
     const token = await user.generateJWT();
+    delete user._doc.password;
     return res
       .status(201)
       .json({ user, token, message: "User created successfully" });
@@ -39,6 +40,7 @@ export const loginUserController = async (req, res) => {
     }
 
     const token = await user.generateJWT();
+    delete user._doc.password;
     return res
       .status(200)
       .json({ user, token, message: "User logged in successfully" });
