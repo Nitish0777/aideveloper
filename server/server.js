f5f5dd1e5c5030580a86a671fb9193eb95d7ee84
Nitish Nashine <nitishnashine@gmail.com>
2025-01-03 09:05:30 +0530

    feat(server): integrate socket.io for real-time communication and add JWT authentication

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
-import 'dotenv/config';
-import http from 'http';
-import app from './app.js';
+import "dotenv/config";
+import http from "http";
+import app from "./app.js";
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.use((socket, next) => {
+    try {
+        const token = socket.handshake?.auth?.token || socket.handshake.headers?.authorization?.split(" ")[1];
+
+        if (!token) {
+            return next(new Error("Unauthorized"));
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            return next(new Error("Unauthorized"));
+        }
+        socket.user = decoded;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+  socket.on("event", (data) => {
+    /* … */
+  });
+  socket.on("disconnect", () => {
+    /* … */
+  });
+});
 
 server.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(`Server is running on port ${port}`);
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server is running on port ${port}`);
 });
